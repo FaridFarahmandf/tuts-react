5a7aa876029ea113428e8e9f97f6b1fd96cf8686
FaridFarahmand <faridfarahmandf2f@gmail.com>
2022-07-05 13:37:59 +0430

    dynamic variables in component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 
 function App() {
+  const title= "Welcome Again";
+  const name= "Farid";
+  const age= 25;
+  const famiyMember=["Farnoosh","Freidoon","Maryam"]
+  const googleLink='http://www.google.com';
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Farid Farahmand
+          {title}
         </p>
+        <h1>{name}</h1>
+        <p>{age}</p>
+        <p>{famiyMember}</p>
+        <a href={googleLink}>Google</a>
       </header>
     </div>
   );
