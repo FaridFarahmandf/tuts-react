1665c8e78cd62e3aada951bee1fae269abd26685
FaridFarahmand <faridfarahmandf2f@gmail.com>
2022-07-06 12:17:10 +0430

    list of states and using map

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,21 @@
 import { useState } from "react";
 const Home = () => {
-    let [name, setName] = useState("Rouzbeh");
-    let [age, setAge] = useState(25)
-    const handleClick = () => {
-        setName("Farid");
-        setAge(32)
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
 
      
     return ( 
-        <>
-            <p style={{fontSize:"2em"}}>Home Page Content</p>
-            <p>{name} is {age} years old</p>
-            <button onClick={handleClick}>Click Here</button>
-        </>
+        <div className="home">
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
+        </div>
      );
 }
  
