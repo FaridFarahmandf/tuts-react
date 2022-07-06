e3a69e2cd9c9b003bf8348827c039279a61b783e
FaridFarahmand <faridfarahmandf2f@gmail.com>
2022-07-06 15:19:31 +0430

    delete handler

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,4 +1,4 @@
-const BlogList = ({blogs,title}) => {
+const BlogList = ({blogs,title,deleteHandle}) => {
     // const blogs = props.blogs;
     // const title = props.title;
     return ( 
@@ -8,6 +8,19 @@ const BlogList = ({blogs,title}) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
+                    <button className="deleteBtn" style={
+                        {
+                            display:"block",
+                            backgroundColor:"#f1356d",
+                            borderRadius:"8px",
+                            color:"#fafafa",
+                            marginTop:"10px",
+                            padding:"10px",
+                            border:"hidden"
+                        }
+                    }
+                    onClick={()=> deleteHandle(blog.id)}
+                    >Delete</button>
                 </div>
             ))}
         </>
--- src/Home.js
+++ src/Home.js
@@ -6,11 +6,17 @@ const Home = () => {
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ])
+    const deleteHandle = (id) => {
+        let newBlogs = blogs.filter(b => b.id !== id);
+        setBlogs(newBlogs);
+    }
 
      
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="Blogs List!"/>
+            <BlogList blogs={blogs} title="Blogs List!" deleteHandle={deleteHandle}/>
+            <BlogList blogs={blogs.filter((blog) => blog.author === "mario")} title="Mario's Blog"/>
+            <BlogList blogs={blogs.filter((blog) => blog.author === "yoshi")} title="Yoshi's Blog"/>
         </div>
      );
 }
