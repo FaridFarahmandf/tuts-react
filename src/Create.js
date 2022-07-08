baf11e81fe49e5907fa7694ed85753e496f3b38a
FaridFarahmand <faridfarahmandf2f@gmail.com>
2022-07-08 12:11:14 +0430

    input controller and using parameter for routing

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,34 @@
+import { useState } from "react";
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('mario');
+    const [body, setBody] = useState('');
     return ( 
-        <div>
+        <div className="create">
             <h2>Create a new blog</h2>
+            <form>
+                <label>Title: </label>
+                <input type="text" 
+                       value={title}
+                       required
+                       onChange={(e) => setTitle(e.target.value)} 
+                />
+                <label>Body: </label>
+                 <textarea
+                 required
+                 value={body}
+                 onChange={(e) => setBody(e.target.value)}>
+                 </textarea>
+                 <label>Author: </label>
+                 <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+                    <option value="mario">Mario</option>
+                    <option value="yoshi">Yoshi</option>
+                 </select>
+                 <button>submit</button>
+            </form>
+            <p>{title}</p>
+            <p>{body}</p>
+            <p>{author}</p>
         </div>
      );
 }
